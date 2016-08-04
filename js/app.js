582e7c25da1a4c9e171ce94d465f8420d6cacd5b
zhaolu <1632089314@qq.com>
2016-08-04 11:25:06 +0800

    state and props

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-// TODO
+const HelloUniverse = React.createClass({
+  getInitialState: function () {
+    return {
+      name: ''
+    };
+  },
+  handleChange: function (event) {
+    this.setState({
+      name: event.target.value
+    });
+  },
+  render: function () {
+    return <div>
+      <HelloWorld name={this.state.name}/>
+      <br/>
+      <input type="text" onChange={this.handleChange}/>
+    </div>
+  },
+});
+
+const HelloWorld = React.createClass({
+  render: function () {
+    return <p>Hello,{this.props.name ? this.props.name : "World"}</p>;
+  },
+});
+
+ReactDOM.render(<HelloUniverse />, document.getElementById('content'));
